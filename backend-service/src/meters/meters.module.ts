200e12480bfa6e4f893df71a5439fc272707cf89
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-03-24 15:32:14 +0200

    feat tests

--- backend-service/src/meters/meters.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MetersService } from './meters.service';
-import { MetersController } from './meters.controller';
-
-@Module({
-  controllers: [MetersController],
-  providers: [MetersService]
-})
-export class MetersModule {}
--- backend-service/src/utils/password.ts
+++
@@ -1,6 +0,0 @@
-import { genSaltSync, hashSync } from 'bcrypt';
-
-export async function hashPassword(password: string) {
-  const salt = genSaltSync(10);
-  return hashSync(password, salt);
-}
