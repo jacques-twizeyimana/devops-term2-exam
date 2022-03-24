5d76150b469f747f2e3b16544a01230e76ced34a
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-03-24 16:26:15 +0200

    feat: use DB from container instead of local one

--- backend-service/src/index.ts
+++ backend-service/src/index.ts
@@ -2,6 +2,6 @@ import app from "./utils/app";
 
 const server = app();
 
-server.listen(8001, () => {
-	console.log("Server started on port 8001");
+server.listen(5000, () => {
+	console.log("Server started on port 5000");
 });
